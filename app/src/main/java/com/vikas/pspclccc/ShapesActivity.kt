5499f532396f4ac5ce6572572e7fc66935a4dc11
vikas kumar <vkbookworm@gmail.com>
2021-05-30 19:20:47 +0530

    feat: added the instagram and facebook and messanger icons

--- app/src/main/java/com/vikas/pspclccc/ShapesActivity.kt
+++ app/src/main/java/com/vikas/pspclccc/ShapesActivity.kt
@@ -1,33 +1,135 @@
 package com.vikas.pspclccc
 
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.vector.DefaultTintBlendMode
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class ShapesActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            drawShapes()
+            Column(
+                Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                instagramIcon()
+                facebookIcon()
+                whatsAppIcon()
+            }
         }
     }
 
+}
+
+@Composable
+fun instagramIcon() {
+    val instaColors = listOf(Color.Yellow, Color.Red, Color.Magenta)
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+    ) {
+        drawRoundRect(
+            brush = Brush.linearGradient(colors = instaColors),
+            cornerRadius = CornerRadius(60f, 60f),
+            style = Stroke(width = 15f, cap = StrokeCap.Round)
+        )
+        drawCircle(
+            brush = Brush.linearGradient(colors = instaColors),
+            radius = 45f,
+            style = Stroke(width = 15f, cap = StrokeCap.Round)
+        )
+        drawCircle(
+            brush = Brush.linearGradient(colors = instaColors),
+            radius = 13f,
+            center = Offset(this.size.width * .80f, this.size.height * 0.20f),
+        )
+    }
+}
+
+@Composable
+fun facebookIcon() {
+    val assetManager = LocalContext.current.assets
+    val paint = Paint().apply {
+        textAlign = Paint.Align.CENTER
+        textSize = 200f
+        color = Color.White.toArgb()
+        typeface = Typeface.createFromAsset(assetManager, "FACEBOLF.OTF")
+    }
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+    ) {
+        drawRoundRect(
+            color = Color(0xFF1776d1),
+            cornerRadius = CornerRadius(20f, 20f)
+        )
+        drawContext.canvas.nativeCanvas.drawText("f", center.x + 25, center.y + 100, paint)
+    }
+}
+
+@Composable
+fun whatsAppIcon() {
+    val colors = listOf(Color(0xFF02b8f9), Color(0xFF0277fe))
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+    ) {
+
+        val trianglePath = Path().let {
+            it.moveTo(this.size.width * .20f, this.size.height * .77f)
+            it.lineTo(this.size.width * .20f, this.size.height * 0.95f)
+            it.lineTo(this.size.width * .37f, this.size.height * 0.86f)
+            it.close()
+            it
+        }
+
+        val electricPath = Path().let {
+            it.moveTo(this.size.width * .20f, this.size.height * 0.60f)
+            it.lineTo(this.size.width * .45f, this.size.height * 0.35f)
+            it.lineTo(this.size.width * 0.56f, this.size.height * 0.46f)
+            it.lineTo(this.size.width * 0.78f, this.size.height * 0.35f)
+            it.lineTo(this.size.width * 0.54f, this.size.height * 0.60f)
+            it.lineTo(this.size.width * 0.43f, this.size.height * 0.45f)
+            it.close()
+            it
+        }
+
+        drawOval(
+            Brush.verticalGradient(colors = colors),
+            size = Size(this.size.width, this.size.height * 0.95f)
+        )
+
+        drawPath(
+            path = trianglePath,
+            Brush.verticalGradient(colors = colors),
+            style = Stroke(width = 15f, cap = StrokeCap.Round)
+        )
+
+        drawPath(path = electricPath, color = Color.White)
+
+    }
 }
 
 @Composable
@@ -93,5 +195,5 @@ fun drawShapes() {
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
 fun showShapesPreview() {
-    drawShapes()
+    instagramIcon()
 }
